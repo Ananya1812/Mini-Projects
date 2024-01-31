da37b53637f034efadf23393e76b1a3bdadec839
Ananya1812 <ananyakini04@gmail.com>
2024-01-31 16:33:58 +0530

    updated

---
+++ 01-E-Book/main/main.js
@@ -0,0 +1,35 @@
+// AUTHOR 1 FUNCTIONAL
+const author1 = document.getElementById("author1");
+author1.onclick = () => {
+  location.href = "./chetan-bhagat/cb.html";
+};
+
+// AUTHOR 2 FUNCTIONAL
+const author2 = document.getElementById("author2");
+author2.onclick = () => {
+  location.href = "./jk-rowling/jk.html";
+};
+
+// AUTHOR 3 FUNCTIONAL
+const author3 = document.getElementById("author3");
+author3.onclick = () => {
+  location.href = "./paulo-coelho/pc.html";
+};
+
+// AUTHOR 4 FUNCTIONAL
+const author4 = document.getElementById("author4");
+author4.onclick = () => {
+  location.href = "./roald-dalh/rd.html";
+};
+
+// AUTHOR 5 FUNCTIONAL
+const author5 = document.getElementById("author5");
+author5.onclick = () => {
+  location.href = "./ruskin-bond/rb.html";
+};
+
+// AUTHOR 6 FUNCTIONAL
+const author6 = document.getElementById("author6");
+author6.onclick = () => {
+  location.href = "./sudha-murthy/sm.html";
+};
